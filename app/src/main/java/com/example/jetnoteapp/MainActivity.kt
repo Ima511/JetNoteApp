674b9694569fca5c365292f8c1e1df3e382c469c
Ima511 <abhaykumarjhaexam420@rediffmail.com>
2023-07-10 17:47:29 +0530

    This commit consist basic architecture of JetNotesApp like It's UI. And some basic functionality of App.

---
+++ app/src/main/java/com/example/jetnoteapp/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.jetnoteapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetnoteapp.data.NotesDataSource
+import com.example.jetnoteapp.model.Note
+import com.example.jetnoteapp.screen.NoteScreen
+import com.example.jetnoteapp.ui.theme.JetNoteAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetNoteAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val notes = remember{
+                        mutableStateListOf<Note>()
+                    }
+                    NoteScreen(
+                        notes = notes,
+                        onRemoveNote = {
+                                       notes.remove(it)
+                        },
+                        onAddNote = {
+                            notes.add(it)
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetNoteAppTheme {
+        Greeting("Android")
+    }
+}
